9b8f3f76d6434abe11d4d3783656717147061d7d
Pietro Annobil <pietroannobil@Pietros-MacBook-Air.local>
2022-05-23 16:26:06 +0100

    init

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import './App.css';
 import {useState,useEffect} from 'react'
 
 function App() {
-  const [name,setName] = useState('')
+  const [myName,setMyName] = useState('')
 const fetchData = async() =>{
   const response = await fetch('https://heroic-madeleine-7b4202.netlify.app/.netlify/functions/hello');
   const data = await response.json()
-  console.log(data)
-  setName(data.name)
+  console.log(data.name)
+  setMyName(data.name)
 }
 useEffect(()=>{
 fetchData();
 },[])
   return (
     <div className="App">
-      <h1>hello {name}!!!!</h1>
+      <h1>hello {myName}!!!!</h1>
     </div>
   );
 }
