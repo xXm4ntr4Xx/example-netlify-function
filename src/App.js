487fc8ac3a258c334c4cc66fc8d8ef58e7f3de58
Pietro Annobil <pietroannobil@Pietros-MacBook-Air.local>
2022-05-23 15:27:08 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import {useState,useEffect} from 'react'
 
 function App() {
+const fetchData = async() =>{
+  const response = await fetch('https://dapper-chimera-3d1b89.netlify.app/.netlify/functions/data');
+  const data = await response.json();
+  console.log(data)
+}
+useEffect(()=>{
+fetchData();
+},[])
   return (
     <div className="App">
-      <h1>hello</h1>
+      
     </div>
   );
 }
