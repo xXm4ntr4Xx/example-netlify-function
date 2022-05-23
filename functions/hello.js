1413772bbbf4ec17eff54bcc3e687b8071903f8e
Pietro Annobil <pietroannobil@Pietros-MacBook-Air.local>
2022-05-23 14:59:41 +0100

    init

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,8 @@
 // functions/hello.js
 export async function handler(event) {
-  const subject = event.queryStringParameters.name || 'World'
+  
   return {
     statusCode: 200,
-    body: `Hello ${subject}!`,
+    body: `Hello Server!`,
   }
 }
