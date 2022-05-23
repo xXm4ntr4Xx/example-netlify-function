f45909e20dd3841ab984cbc31dacdd93987c370a
Pietro Annobil <pietroannobil@Pietros-MacBook-Air.local>
2022-05-23 15:48:45 +0100

    init

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import {useEffect} from 'react'
 
 function App() {
 const fetchData = async() =>{
-  const response = await fetch('https://heroic-madeleine-7b4202.netlify.app/.netlify/functions/data');
+  const response = await fetch('https://heroic-madeleine-7b4202.netlify.app/.netlify/functions/hello');
   
-  console.log(response.body)
+  console.log(response)
 }
 useEffect(()=>{
 fetchData();
